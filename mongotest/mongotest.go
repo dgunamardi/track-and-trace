4afc9957269df8a4d0c3d69692d653460c7352a4
tkgoh <dariangunamardi@gmail.com>
2022-08-16 16:41:00 +0700

    added mongotest

---
+++ mongotest/mongotest.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	URI = "mongodb://localhost:27017/track_trace"
+)
+
+func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(URI))
+	if err != nil {
+		panic(fmt.Errorf("failed to create client: %v", err))
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+}
